0c30778843a611f6c278085f13bba4b23f7389d1
jsh <jsh@LAPTOP-GSIC3COA>
2018-03-04 03:47:36 +0900

    https://github.com/jsh2174/exBit.git

--- aaa/src/aaa/asad.java
+++ aaa/src/aaa/asad.java
@@ -5,11 +5,7 @@ public class asad {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println( "Hello World!" );
-	     System.out.println( "Hello World!" );
-	     System.out.println( "Hello World!" );
-	        System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
-	        System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
-	        System.out.println( "Hello World!" );
+	  
 	}
 
 }
